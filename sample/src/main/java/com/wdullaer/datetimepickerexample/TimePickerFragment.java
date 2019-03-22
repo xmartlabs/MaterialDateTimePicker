60c54e61a131ca6d4d9d6e15e49530f28dada4ca
Bruno Ferrari <bruno@xmartlabs.com>
2019-03-22 12:01:03 -0300

    Changed from app.FragmentManager to support fragment manager

--- sample/src/main/java/com/wdullaer/datetimepickerexample/TimePickerFragment.java
+++ sample/src/main/java/com/wdullaer/datetimepickerexample/TimePickerFragment.java
@@ -1,9 +1,9 @@
 package com.wdullaer.datetimepickerexample;
 
-import android.app.Fragment;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
